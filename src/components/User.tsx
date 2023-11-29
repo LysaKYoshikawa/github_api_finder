16c321eff7da6da98bec81deff4416a575f088eb
Monalysa <monalysaklauck@gmail.com>
2023-11-28 23:53:18 -0300

    feat:add app finder

---
+++ src/components/User.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { UserProps } from '../types/user';
+import { MdLocationPin } from "react-icons/md";
+import {Link} from 'react-router-dom';
+import classes from "./User.module.css";
+
+export const User = ({
+    location, login, avatar_url, followers, following,}: UserProps) => {
+  return (
+    <div className={classes.user}>
+        <img src={avatar_url} alt={login}/>
+        <h2>{login}</h2>
+        {location &&(
+            <p className={classes.location}>
+                <MdLocationPin/>
+                <span>{location}</span>
+            </p>
+        )}
+        
+        <div className={classes.stats}>
+            <div>
+                <p>Seguidores:</p>
+                <p className={classes.number}>{followers}</p>
+            </div>
+            <div>
+                <p>Seguindo:</p>
+                <p className={classes.number}>{following}</p>
+            </div>
+
+            <Link target="_blank" to={`https://github.com/${login}`}>Ver melhores projetos</Link>
+        </div>
+
+
+    </div>
+  );
+};
